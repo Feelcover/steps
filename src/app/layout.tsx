519c42c463158a4f7156c5c25084915de9ba7f3b
Andrey Chebanov <pioneerbeat@yandex.ru>
2024-03-26 18:57:26 +0300

    добавить arrow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
 import "../styles/globals.scss";
 
-
-
 export const metadata: Metadata = {
   title: "Steps",
-  description: "Steps test task",
+  description: "Steps test task"
 };
 
 export default function RootLayout({
@@ -14,6 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
     <html lang="en">
       <body>{children}</body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,18 @@ import styles from "../styles/page.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <div className={styles.wrapper}>
-      <h1 className={styles.page__title}>Зарабатывайте вместе с нами!</h1>
-        <div className={styles.container}>
-          <main className={styles.page__main}>
-            <Card link="../../images/partnerCard.svg"/>
-            <Card link="../../images/repostCard.svg" />
-            <Card link="../../images/persentCard.svg"/>
-          </main>
-          <Button text="Стать партнером" marginRight={191}/>
+      <div className={styles.page}>
+        <div className={styles.wrapper}>
+          <h1 className={styles.page__title}>Зарабатывайте вместе с нами!</h1>
+          <div className={styles.container}>
+            <main className={styles.page__main}>
+              <Card link="../../images/partnerCard.svg" />
+              <Card link="../../images/repostCard.svg" />
+              <Card link="../../images/persentCard.svg" />
+            </main>
+            <Button text="Стать партнером" marginRight={191} />
+          </div>
         </div>
       </div>
-    </div>
   );
 }
