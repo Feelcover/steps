c35e3b592e3955c76b0b23452adcc08f1d970a1a
Andrey Chebanov <pioneerbeat@yandex.ru>
2024-03-26 21:43:14 +0300

    изменение версии next, анимации и часть мобильной версии

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,36 @@
+import { useMediaQuery } from "@/src/hooks/useWindowWidth";
 import React, { FC } from "react";
 import styles from "./Card.module.scss";
 
 interface ICard {
-  link: string;
+  partnerCard?: boolean;
+  repostCard?: boolean;
+  persentCard?: boolean;
+  marginTop?: number;
 }
 
-const Card: FC<ICard> = ({ link }) => {
+const Card: FC<ICard> = ({ partnerCard, repostCard, persentCard, marginTop }) => {
+  const isMobile = useMediaQuery(1130);
+  let partnerCardItem = isMobile
+    ? "../../images/partnerCardMob.svg"
+    : "../../images/partnerCard.svg";
+  let repostCardItem = isMobile
+    ? "../../images/repostCardMob.svg"
+    : "../../images/repostCard.svg";
+  let persentCardItem = isMobile
+    ? "../../images/persentCardMob.svg"
+    : "../../images/persentCard.svg";
+
   return (
     <div
       className={styles.card}
       style={{
-        backgroundImage: `url(${link})`,
+        backgroundImage: `url(${
+          (partnerCard && partnerCardItem) ||
+          (repostCard && repostCardItem) ||
+          (persentCard && persentCardItem)
+        })`,
+        marginTop
       }}
     ></div>
   );
---
+++ src/pages/index.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import Card from "../components/Card/Card";
+import Button from "../components/Button/Button";
+import styles from "../styles/page.module.scss";
+import Head from "next/head";
+import { motion } from "framer-motion";
+import { useMediaQuery } from "../hooks/useWindowWidth";
+
+export default function Home() {
+  const isMobile = useMediaQuery(1130);
+  let marginTopCards = isMobile ? -40 : 0;
+  return (
+    <>
+      <Head>
+        <title>Steps</title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div className={styles.page}>
+        <div className={styles.wrapper}>
+          <motion.div
+            transition={{
+              delay: 1.5,
+              ease: "linear",
+              duration: 0.5,
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <h1 className={styles.page__title}>Зарабатывайте вместе с нами!</h1>
+          </motion.div>
+          <div className={styles.container}>
+            <motion.div
+              transition={{
+                delay: 1,
+                ease: "linear",
+                duration: 0.5,
+              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+            >
+              <main className={styles.page__main}>
+                <motion.div
+                  transition={{
+                    delay: 1,
+                    ease: "linear",
+                    duration: 0.5,
+                  }}
+                  initial={{ opacity: 0, y: -120 }}
+                  animate={{ opacity: 1, y: 0 }}
+                >
+                  <Card marginTop={marginTopCards} partnerCard />
+                </motion.div>
+                <motion.div
+                  transition={{
+                    delay: 1.2,
+                    ease: "linear",
+                    duration: 0.5,
+                  }}
+                  initial={{ opacity: 0, y: -120 }}
+                  animate={{ opacity: 1, y: 0 }}
+                >
+                  <Card marginTop={marginTopCards} repostCard />
+                </motion.div>
+                <motion.div
+                  transition={{
+                    delay: 1.4,
+                    ease: "linear",
+                    duration: 0.5,
+                  }}
+                  initial={{ opacity: 0, y: -120 }}
+                  animate={{ opacity: 1, y: 0 }}
+                >
+                  <Card marginTop={marginTopCards} persentCard />
+                </motion.div>
+              </main>
+            </motion.div>
+            <motion.div
+              transition={{
+                delay: 1.7,
+                ease: "linear",
+                duration: 0.5,
+              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+            >
+              <Button text="Стать партнером" marginRight={191} />
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
